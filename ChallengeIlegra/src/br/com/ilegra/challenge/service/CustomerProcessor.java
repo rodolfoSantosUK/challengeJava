031015a5fa978749eb3b05722d2346f68a98ca76
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-10-21 14:21:49 -0300

    [FIX] Changing the class name.

---
+++ ChallengeIlegra/src/br/com/ilegra/challenge/service/CustomerProcessor.java
@@ -0,0 +1,16 @@
+package br.com.ilegra.challenge.service;
+
+import java.util.Set;
+
+public class CustomerProcessor extends RevenueProcessor<CustomerProcessor> {
+
+
+    @Override
+    public Long processAmount(Set<CustomerProcessor> set) {
+        return super.processAmount(set);
+    }
+
+    public CustomerProcessor() {
+
+    }
+}
--- ChallengeIlegra/src/br/com/ilegra/challenge/test/TestChallengeIlegra.java
+++ ChallengeIlegra/src/br/com/ilegra/challenge/test/TestChallengeIlegra.java
@@ -1,7 +1,14 @@
 package br.com.ilegra.challenge.test;
 
 
+import br.com.ilegra.challenge.model.Revenue;
+import br.com.ilegra.challenge.model.Salesman;
+import br.com.ilegra.challenge.service.SalesmanProcessor;
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 
 public class TestChallengeIlegra {
@@ -10,14 +17,19 @@ public class TestChallengeIlegra {
     @Test
     public void mustProcessSalesmanAmount() {
 
+        Set<Salesman> salesmanSet = new HashSet<Salesman>();
 
+        Salesman salesman = new Salesman("001", "Rodolfo", "10677061781", 150.50);
+        Salesman salesman2 = new Salesman("001", "Ronaldo", "10204587542", 120.50);
 
-      assertEquals(1,1);
+        salesmanSet.add(salesman);
+        salesmanSet.add(salesman2);
+
+        Revenue<Salesman> revenueProcess =  new SalesmanProcessor();
+        revenueProcess.processAmount(salesmanSet);
+
+        assertEquals(2, revenueProcess.processAmount(salesmanSet).longValue());
     }
 
-
-    public static void main(String[] args) {
-        System.out.println("foi");
-    }
 
 }
